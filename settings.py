dc3ca74791c677e39ab951ebfcc07ac076bcfee6
Williames Camilo <williamescw@gmail.com>
2026-04-09 12:24:03 -0300

    Modificando as configurações de velocidade do jogo

--- settings.py
+++ settings.py
@@ -22,7 +22,23 @@ class Settings():
         self.alien_speed_factor = 0.1
         self.fleet_drop_speed = 10
         
+        # A taxa com que a velocidade do jogo aumenta
+        self.speedup_scale = 1.1
+        
         # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
         self.fleet_direction = 1
-
+        
+    def initialize_dynamic_settings(self): 
+        """Inicializa as configurações que mudam no decorrer do jogo."""
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
+        # fleet_direction igual a 1 representa a direita; -1 representa a esquerda
+        self.fleet_direction = 1
+        
+    def increase_speed(self): 
+        """Aumenta as configurações de velocidade."""
+        self.ship_speed_factor *= self.speedup_scale 
+        self.bullet_speed_factor *= self.speedup_scale 
+        self.alien_speed_factor *= self.speedup_scale
         
